f6d841dc927eccb7085d2b157925b111b0f1905c
jinsejin <j03298969@gmail.com>
2023-02-17 13:47:50 +0900

    fianl project 1

---
+++ dariyproject/lib/fontstyle.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const TextStyle fontboldbold = TextStyle(
+  //fontFamily: 'Pretendard',
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+const TextStyle fontboldmiddle = TextStyle(
+  //fontFamily: 'Pretendard',
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+const TextStyle fontboldsmall = TextStyle(
+  //fontFamily: 'Pretendard',
+  fontSize: 13,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+const TextStyle fontsmiddle = TextStyle(
+  //fontFamily: 'Pretendard',
+  fontSize: 12,
+  color: Colors.white,
+);
---
+++ dariyproject/lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'mypage.dart';
+import 'write.dart';
+import 'moa.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const Homepage());
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff0A0028),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff0A0028),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Mypage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                '김태희님 ❤ 송하연님' '\n\n' '       함께 기록한지' '\n' '      400(변수)일째',
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 350,
+                width: 300,
+                child: Image.asset(
+                  'assets/images/메인 행성.png',
+                ),
+              ),
+              SizedBox(
+                child: OutlinedButton.icon(
+                  style: OutlinedButton.styleFrom(
+                      side: const BorderSide(
+                          width: 2.0, color: Color(0xffF6BDE5)),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(27.5))),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Write(),
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.drive_file_rename_outline_outlined,
+                      size: 30, color: Colors.white),
+                  label: const Padding(
+                    padding: EdgeInsets.all(7.0),
+                    child: Text(
+                      "교환일기 작성하기",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+                height: 20,
+              ),
+              SizedBox(
+                child: OutlinedButton.icon(
+                  style: OutlinedButton.styleFrom(
+                      side: const BorderSide(
+                          width: 2.0, color: Color(0xffF6BDE5)),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(27.5))),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Moa(),
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.format_list_bulleted,
+                      size: 30, color: Colors.white),
+                  label: const Padding(
+                    padding: EdgeInsets.all(7.0),
+                    child: Text(
+                      "교환일기 모아보기",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
